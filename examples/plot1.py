56c66e734b31a4b396c3a7e450b6d2fc8e4e4297
Hung Dang <hungptit@gmail.com>
2018-10-28 00:08:49 -0400

    Add a simple plot example.

--- examples/plot1.py
+++ examples/plot1.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-import numpy as np
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+from scipy.stats import norm
+import numpy as np
+
 mu, sigma = 100, 15
-x = mu + sigma*np.random.randn(10000)
+x = mu + sigma * np.random.randn(10000)
+
 # the histogram of the data
-n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green',
-alpha=0.75)
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='green', alpha=0.75)
+
 # add a 'best fit' line
-y = mlab.normpdf( bins, mu, sigma)
+y = norm.pdf(bins, mu, sigma)
 l = plt.plot(bins, y, 'r--', linewidth=1)
 plt.xlabel('Smarts')
-plt.ylabel('Probability')
+plt.ylabel('My first plot')
 plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
 plt.axis([40, 160, 0, 0.03])
 plt.grid(True)
